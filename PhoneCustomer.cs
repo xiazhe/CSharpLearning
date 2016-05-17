559aa3483098a6ea29eab4b161befca48f059cba
Zhe Xia <xiazhe@outlook.com>
2016-05-17 15:12:52 +0800

    added PhoneCustomer

---
+++ PhoneCustomer.cs
@@ -0,0 +1,12 @@
+class PhoneCustomer
+{
+    public const string DayOfSendingBill = "Monday";
+    public int CustomerID;
+    public string FirstName;
+    public string LastName;    
+}
+
+
+
+PhoneCustomer Customer1 = new PhoneCustomer();
+Customer1.FirstName = "Simon";
