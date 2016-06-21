4a6099b4a38bf77298b6b32c207ea93a7561c601
Lucker Xia <lucker.xia@missionsky.com>
2016-06-21 17:11:46 +0800

    added timer test

---
+++ Timer/Timer/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Timer
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        //[STAThread]
+        //static void Main()
+        //{
+        //    Application.EnableVisualStyles();
+        //    Application.SetCompatibleTextRenderingDefault(false);
+        //    Application.Run(new Form1());
+        //}
+
+        /// <summary>
+        /// Timer Class
+        /// 实现按用户定义的时间间隔引发事件的计时器。此计时器最宜用于 Windows 窗体应用程序中，并且必须在窗口中使用。
+        /// </summary>
+        static System.Windows.Forms.Timer myTimer = new System.Windows.Forms.Timer();
+        static int alarmCounter = 1;
+        static bool exitFlag = false;
+
+        // This is the method to run when the timer is raised.
+        private static void TimerEventProcessor(Object myObject,
+                                                EventArgs myEventArgs)
+        {
+            myTimer.Stop();
+
+            // Displays a message box asking whether to continue running the timer.
+            if (MessageBox.Show("Continue running?", "Count is: " + alarmCounter,
+               MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                // Restarts the timer and increments the counter.
+                alarmCounter += 1;
+                myTimer.Enabled = true;
+            }
+            else
+            {
+                // Stops the timer.
+                exitFlag = true;
+            }
+        }
+
+        public static int Main()
+        {
+            /* Adds the event and the event handler for the method that will 
+               process the timer event to the timer. */
+            myTimer.Tick += new EventHandler(TimerEventProcessor);
+
+            // Sets the timer interval to 5 seconds.
+            myTimer.Interval = 5000;
+            myTimer.Start();
+
+            // Runs the timer, and raises the event.
+            while (exitFlag == false)
+            {
+                // Processes all the events in the queue.
+                Application.DoEvents();
+            }
+            return 0;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
