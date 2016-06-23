279bb443af933319b27c273fab3dd1e8d936f0ab
Lucker Xia <lucker.xia@missionsky.com>
2016-06-23 13:37:06 +0800

    updated dataset test

--- DataSetTest/DataSetTest/Program.cs
+++ DataSetTest/DataSetTest/Program.cs
@@ -11,11 +11,36 @@ namespace DataSetTest
     {
         static void Main(string[] args)
         {
-            DataSet[] dataSet = null;
-            DataSet dt = new DataSet();
+            //DataSet[] dataSet = null;
+            //DataSet dt = new DataSet();
+
+            //Console.WriteLine(dt.IsInitialized);
+
+            DataSet customerOrders = new DataSet("CustomerOrders");
+
+            DataTable ordersTable = customerOrders.Tables.Add("Orders");
+
+            //主键列
+            DataColumn pkOrderID = ordersTable.Columns.Add("OrderID", typeof(Int32));
+            ordersTable.Columns.Add("OrderQuantity", typeof(Int32));
+            ordersTable.Columns.Add("CompanyName", typeof(string));
+
+            ordersTable.PrimaryKey = new DataColumn[] { pkOrderID };
 
 
-            Console.WriteLine(dataSet[0].IsInitialized);
+            //添加数据
+            ordersTable.Rows.Add(1, 0, "Microsoft");
+            ordersTable.Rows.Add(2, 10, "Google Inc.");
+            ordersTable.Rows.Add(3, 100, "Tea.Fish");
+
+            PrintRows(customerOrders);
+            
+
+
+            Console.WriteLine();
+
+
+
 
 
         }
@@ -27,7 +52,7 @@ namespace DataSetTest
         /// https://msdn.microsoft.com/zh-cn/library/system.data.dataset.tables(v=vs.110).aspx
         /// </summary>
         /// <param name="dataSet"></param>
-        private void PrintRows(DataSet dataSet)
+        private static void PrintRows(DataSet dataSet)
         {
             // For each table in the DataSet, print the row values.
             foreach (DataTable table in dataSet.Tables)
